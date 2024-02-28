be5bb0ca6b183d27197b659e20544a27ea3a52c4
N0rhan <96006888+N0rhan@users.noreply.github.com>
2024-02-28 20:23:24 +0200

    Update Fuzzer.py

--- Fuzzer.py
+++ Fuzzer.py
@@ -1,15 +1,22 @@
 # Simple HTTP Fuzzer 
 import requests
-
+import threading
 wordlist = open('wordlist.txt','r')
 wlist = wordlist.read().splitlines()
+
+def Requested(url) :
+    response = requests.get(url)
+    if response.status_code == 200:
+        print(f"Response from {url}")
+        print(response.content)
+  
+threads = []
+
 for path in wlist:
     url = "http://35.93.142.191" + '/' + path 
-    try:
-        response = requests.get(url)
-        if response.status_code == 200:
-            print(f"Response from {url}: Status 200")
-            print(response.content)
-        print(f"Response from {url}: Status {response.status_code}")
-    except:
-        print(f"Error connecting to {url}")
+    thread = threading.Thread(target=Requested,args=[url])  
+    thread.start()
+    threads.append(thread)
+
+for thread in threads :
+    thread.join()
